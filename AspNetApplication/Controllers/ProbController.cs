147b9b6686e71f6215a54d21fe3e968b29d96610
shibayan <me@shibayan.jp>
2017-07-11 22:46:04 +0900

    add prob

---
+++ AspNetApplication/Controllers/ProbController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace AspNetApplication.Controllers
+{
+    public class ProbController : Controller
+    {
+        // GET: Prob
+        public ActionResult Index()
+        {
+            return Content("OK");
+        }
+    }
+}
