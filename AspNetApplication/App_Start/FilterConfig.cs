666c6f09f25aea59dc1c0853a0e40202e4e58472
shibayan <me@shibayan.jp>
2017-07-11 15:49:13 +0900

    application insights を追加

--- AspNetApplication/App_Start/FilterConfig.cs
+++ AspNetApplication/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace AspNetApplication
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new ErrorHandler.AiHandleErrorAttribute());
         }
     }
 }
