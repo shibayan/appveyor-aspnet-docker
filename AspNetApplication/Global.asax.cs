8365e41c11cedfd6d1e4d0b21e5344a127b8a313
shibayan <me@shibayan.jp>
2019-03-29 19:08:14 +0900

    Remove WEBSITE_SITE_NAME envvar

--- AspNetApplication/Global.asax.cs
+++ AspNetApplication/Global.asax.cs
@@ -1,6 +1,10 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+
+[assembly: PreApplicationStartMethod(typeof(AspNetApplication.CounterInjector), "Start")]
 
 namespace AspNetApplication
 {
@@ -12,6 +16,14 @@ namespace AspNetApplication
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+
+    public static class CounterInjector
+    {
+        public static void Start()
+        {
+            Environment.SetEnvironmentVariable("WEBSITE_SITE_NAME", "");
         }
     }
 }
